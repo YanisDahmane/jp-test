474808fa42e98ca4e07d4508845a2d3b86467d63
YanisDahmane <113184746+YanisDahmane@users.noreply.github.com>
2024-09-13 12:57:44 +0200

    Create relation on Item and Market

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  belongs_to :market
   validates :name, presence: true, uniqueness: true
   validates :code, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than: 0 }
--- app/models/market.rb
+++ app/models/market.rb
@@ -1,3 +1,4 @@
 class Market < ApplicationRecord
+  has_many :items, dependent: :destroy
   validates :name, presence: true, uniqueness: true
 end
