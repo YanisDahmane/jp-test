0c05bbfcf2d637d3bcc94dd5f6a0bdb5fec9831b
YanisDahmane <113184746+YanisDahmane@users.noreply.github.com>
2024-09-13 13:19:02 +0200

    Create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning database..."
+Market.destroy_all
+
+puts "Creating markets..."
+Market.create!(name: "Chez Yanis")
+Market.create!(name: "Chez Momo")
+
+puts "Created #{Market.count} markets."
+puts "Creating items for market"
+
+Market.all.each do |market|
+  10.times do |i|
+    Item.create!(
+      name: "#{Faker::Food.fruits}_#{market.id}_#{i}",
+      code: "#{market.name}_#{i}",
+      price: rand(1.0..10.0),
+      market: market
+    )
+  end
+  puts "Created 10 items for market #{market.name}"
+end
