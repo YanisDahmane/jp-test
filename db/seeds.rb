c1b07f86031a54f644384e87f7c4c49008c880c3
YanisDahmane <113184746+YanisDahmane@users.noreply.github.com>
2024-09-13 15:40:47 +0200

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,19 +3,28 @@ Market.destroy_all
 
 puts "Creating markets..."
 Market.create!(name: "Chez Yanis")
-Market.create!(name: "Chez Momo")
 
 puts "Created #{Market.count} markets."
 puts "Creating items for market"
 
 Market.all.each do |market|
-  10.times do |i|
-    Item.create!(
-      name: "#{Faker::Food.fruits}_#{market.id}_#{i}",
-      code: "#{market.name}_#{i}",
-      price: rand(1.0..10.0),
-      market: market
-    )
-  end
-  puts "Created 10 items for market #{market.name}"
+  Item.create!(
+    name: "Apple",
+    code: "AP1",
+    price: 5,
+    market: market
+  )
+  Item.create!(
+    name: "Fruit Tea",
+    code: "FR1",
+    price: 3.11,
+    market: market
+  )
+  Item.create!(
+    name: "Coffee",
+    code: "CF1",
+    price: 11.23,
+    market: market
+  )
+  puts "Created items for market #{market.name}"
 end
